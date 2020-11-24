1f5c19920450a9377af549fcfa182ac070285564
matter92 <mterenzi@deloitte.it>
2020-11-24 01:50:05 +0100

    fix install deps

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name="stream-tools",
-    version="0.2",
+    version="0.3",
     author="Mattia Terenzi",
     packages=find_packages(),
     install_requires=["aioredis==1.3.1", "uvloop==0.14.0", "numpy==1.19.4"],
